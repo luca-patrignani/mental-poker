614d1410f36bf25fdef8c46bf7bb4a03390b2c28
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2024-12-08 22:18:34 +0100

    Implement broadcast

--- common/broadcast.go
+++ common/broadcast.go
@@ -1,21 +1,50 @@
 package common
 
+import (
+	"net"
+	"time"
+)
+
 type Player struct {
-	MyRank int
+	Rank int
+	Addresses []string
 }
 
-func (p Player) BroadcastAllToAll(bufferSend string) (bufferRecv []string) {
+func (p Player) BroadcastAllToAll(bufferSend string) (bufferRecv []string, err error) {
 	bufferRecv = nil
 	return
 }
 
-func (p Player) Broadcast(bufferSend string, root int) (bufferRecv string, err error) {
-	if root == p.MyRank {
-		bufferRecv = bufferSend
-		// send
-	} else {
-		bufferRecv = ""
-		// receive
+func (p Player) Broadcast(bufferSend string, root int) (string, error) {
+	if root == p.Rank {
+		for i, addr := range p.Addresses {
+			if i != p.Rank {
+				conn, err := net.Dial("tcp", addr)
+				for err != nil {
+					time.Sleep(time.Millisecond)
+					conn, err = net.Dial("tcp", addr)
+				}
+				_, err = conn.Write([]byte(bufferSend))
+				if err != nil {
+					return "", err
+				}
+			}
+		}
+		return bufferSend, nil
 	}
-	return
+	listener, err := net.Listen("tcp", p.Addresses[p.Rank])
+	if err != nil {
+		return "", err
+	}
+	conn, err := listener.Accept()
+	if err != nil {
+		return "", err
+	}
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+	buffer = buffer[:n]
+	return string(buffer), nil
 }
--- common/broadcast_test.go
+++ common/broadcast_test.go
@@ -1,16 +1,20 @@
 package common
 
 import (
+	"fmt"
 	"strconv"
-	"testing"
 	"sync"
+	"testing"
 )
 
 func Test(t *testing.T) {
 	b1 := Player{}
 	b2 := Player{}
 	go b1.BroadcastAllToAll("a1")
-	actual := b2.BroadcastAllToAll("a2")
+	actual, err := b2.BroadcastAllToAll("a2")
+	if err != nil {
+		t.Fatal(err)
+	}
 	if len(actual) != 2 {
 		t.Fatal()
 	}
@@ -28,7 +32,10 @@ func Test3(t *testing.T) {
 	b3 := Player{}
 	go b1.BroadcastAllToAll("a1")
 	go b3.BroadcastAllToAll("a3")
-	actual := b2.BroadcastAllToAll("a2")
+	actual, err := b2.BroadcastAllToAll("a2")
+	if err != nil {
+		t.Fatal(err)
+	}
 	if len(actual) != 3 {
 		t.Fatal()
 	}
@@ -38,21 +45,32 @@ func Test3(t *testing.T) {
 }
 
 func Test4(t *testing.T) {
-	root := 0
+	addresses := []string{}
+	for i := 0; i < 10; i++ {
+		addresses = append(addresses, fmt.Sprintf("127.0.0.1:111%d", i))
+	}
+	root := 3
 	fatal := make(chan error, 10)
 	var wg sync.WaitGroup
 	wg.Add(10)
 	for i := 0; i < 10; i++ {
 		go func(i int) {
 			defer wg.Done()
-			b := Player{MyRank: i}
+			p := Player{Rank: i, Addresses: addresses}
 			a := strconv.Itoa(10 * i)
-			recv, err := b.Broadcast(a, root)
+			recv, err := p.Broadcast(a, root)
 			if err != nil {
 				fatal <- err
+				return
 			}
-			if recv != strconv.Itoa(root*10) {
+			actual, err := strconv.Atoi(recv)
+			if err != nil {
 				fatal <- err
+				return
+			}
+			if actual != root*10 {
+				fatal <- fmt.Errorf("expected %d, actual %d", actual, root*10)
+				return
 			}
 		}(i)
 	}
