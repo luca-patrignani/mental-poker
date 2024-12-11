7db9179d175f3e6edcdc7652bba27fd6fcb9e9d1
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2024-12-11 18:18:14 +0100

    Try fix broadcast and all to all

--- common/broadcast.go
+++ common/broadcast.go
@@ -1,8 +1,13 @@
 package common
 
 import (
-	"bufio"
+	"context"
+	"fmt"
+	"io"
 	"net"
+	"net/http"
+	"slices"
+	"strings"
 	"time"
 )
 
@@ -11,22 +16,101 @@ import (
 // Addresses[i] contains the address to reach the Player with Rank i.
 type Player struct {
 	Rank      int
-	Addresses []string
+	Addresses []net.TCPAddr
+	// listener net.Listener
+}
+
+func NewPlayer(rank int, addresses []net.TCPAddr) (Player, error) {
+	// listener, err := net.Listen("tcp", addresses[rank])
+	// if err != nil {
+	// 	return Player{}, err
+	// }
+	p := Player{
+		Rank:      rank,
+		Addresses: addresses,
+		// listener: listener,
+	}
+	return p, nil
 }
 
 // Each caller of AllToAll sends the content of bufferSend to every node.
 // bufferRecv[i] will contain the value sent by the Player with Rank i.
 // This function will implicitly synchronize the players.
-func (p Player) AllToAll(bufferSend string) (bufferRecv []string, err error) {
-	bufferRecv = make([]string, len(p.Addresses))
-	for i := 0; i < len(p.Addresses); i++ {
-		recv, err := p.broadcastNoBarrier(bufferSend, i)
+func (p Player) AllToAll(bufferSend []byte) (bufferRecv [][]byte, err error) {
+	bufferRecv = make([][]byte, len(p.Addresses))
+	bufferRecv[p.Rank] = bufferSend
+	s := http.ServeMux{}
+	fatal := make(chan error)
+	s.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		remoteAddr, err := net.ResolveTCPAddr("tcp", r.RemoteAddr)
 		if err != nil {
-			return nil, err
+			fatal <- err
+			return
+		}
+		i := -1
+		for j := 0; j < len(p.Addresses); j++ {
+			if tcpAddrEqual(p.Addresses[j], *remoteAddr) {
+				i = j
+				break
+			}
+		}
+		recv, err := io.ReadAll(r.Body)
+		if err != nil {
+			fatal <- err
+			return
 		}
 		bufferRecv[i] = recv
+		w.WriteHeader(http.StatusAccepted)
+	})
+	// Create a custom transport with a DialContext
+	transport := &http.Transport{
+		DialContext: (&net.Dialer{
+			LocalAddr: &p.Addresses[p.Rank], // Set the local address
+		}).DialContext,
+	}
+	// Create an HTTP client with the custom transport
+	client := &http.Client{
+		Transport: transport,
+	}
+	for i, addr := range p.Addresses {
+		if i != p.Rank {
+			resp, err := client.Post("http://"+addr.String(), "application/octet-stream", strings.NewReader(string(bufferSend)))
+			for err != nil || resp.StatusCode != http.StatusAccepted {
+				resp, err = client.Post("http://"+addr.String(), "application/octet-stream", strings.NewReader(string(bufferSend)))
+			}
+		}
+	}
+
+	for slices.ContainsFunc(bufferRecv, func(b []byte) bool { return b == nil }) {
 	}
 	return
+}
+
+type myHandler struct {
+	RootAddr       net.TCPAddr
+	ContentChannel chan []byte
+	ErrChannel     chan error
+}
+
+func (h *myHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	remoteAddr, err := net.ResolveTCPAddr("tcp", req.RemoteAddr)
+	if err != nil {
+		rw.WriteHeader(http.StatusNotAcceptable)
+		h.ErrChannel <- fmt.Errorf("from handler: %v", err)
+		return
+	}
+	if !tcpAddrEqual(*remoteAddr, h.RootAddr) {
+		rw.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	content, err := io.ReadAll(req.Body)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		h.ErrChannel <- fmt.Errorf("from handler: %v", err)
+		return
+	}
+	h.ContentChannel <- content
+	rw.WriteHeader(http.StatusAccepted)
 }
 
 // The Player with Rank root sends the content of bufferSend to every node.
@@ -48,7 +132,7 @@ func (p Player) Broadcast(bufferSend string, root int) (string, error) {
 // In particular this method guarantees that no Player's control flow will
 // leave this function until every player has entered this function.
 func (p Player) barrier() error {
-	_, err := p.AllToAll("")
+	_, err := p.AllToAll(nil)
 	if err != nil {
 		return err
 	}
@@ -58,39 +142,60 @@ func (p Player) barrier() error {
 // The Player with Rank root sends the content of bufferSend to every node.
 // bufferRecv will contain the value sent by the Player with Rank root.
 func (p Player) broadcastNoBarrier(bufferSend string, root int) (string, error) {
-	const delim byte = 0x0
 	if root == p.Rank {
+		// Create a custom transport with a DialContext
+		transport := &http.Transport{
+			DialContext: (&net.Dialer{
+				LocalAddr: &p.Addresses[p.Rank], // Set the local address
+			}).DialContext,
+		}
+		// Create an HTTP client with the custom transport
+		client := &http.Client{
+			Transport: transport,
+		}
 		for i, addr := range p.Addresses {
 			if i != p.Rank {
-				conn, err := net.Dial("tcp", addr)
-				for err != nil {
+				resp, err := client.Post("http://"+addr.String(), "application/octet-stream", strings.NewReader(bufferSend))
+				for err != nil || resp.StatusCode != http.StatusAccepted {
 					time.Sleep(time.Millisecond)
-					conn, err = net.Dial("tcp", addr)
+					resp, err = client.Post("http://"+addr.String(), "application/octet-stream", strings.NewReader(bufferSend))
 				}
-				defer conn.Close()
-				_, err = conn.Write(append([]byte(bufferSend), delim))
-				if err != nil {
-					return "", err
+				if resp.StatusCode != http.StatusAccepted {
+					return "", fmt.Errorf("unsuccessful status code %d", resp.StatusCode)
 				}
 			}
 		}
 		return bufferSend, nil
 	}
-	listener, err := net.Listen("tcp", p.Addresses[p.Rank])
-	if err != nil {
-		return "", err
+	errChan := make(chan error)
+	handler := myHandler{
+		RootAddr:       p.Addresses[root],
+		ContentChannel: make(chan []byte),
+		ErrChannel:     errChan,
 	}
-	conn, err := listener.Accept()
-	if err != nil {
-		return "", err
+	s := http.Server{
+		Addr:        p.Addresses[p.Rank].AddrPort().String(),
+		Handler:     &handler,
+		IdleTimeout: time.Second,
 	}
-	listener.Close()
-	defer conn.Close()
-	reader := bufio.NewReader(conn)
-	line, err := reader.ReadString(delim)
-	if err != nil {
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			errChan <- err
+			return
+		}
+	}()
+	var recv []byte
+	select {
+	case err := <-errChan:
 		return "", err
+	case recv = <-handler.ContentChannel:
+		break
 	}
-	line = line[:len(line)-1]
-	return line, nil
+	s.Shutdown(context.Background())
+	return string(recv), nil
+}
+
+func tcpAddrEqual(a, b net.TCPAddr) bool {
+	return a.IP.Equal(b.IP) && a.Port == b.Port
 }
--- common/broadcast_test.go
+++ common/broadcast_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net"
 	"strconv"
-	"strings"
 	"sync"
 	"testing"
 	"time"
@@ -14,8 +13,8 @@ func createAddresses(n int) []net.TCPAddr {
 	addresses := []net.TCPAddr{}
 	for i := 0; i < n; i++ {
 		addresses = append(addresses, net.TCPAddr{
-			IP: net.ParseIP("127.0.0.1"),
-			Port: 50000+i,
+			IP:   net.ParseIP("127.0.0.1"),
+			Port: 50000 + i,
 		})
 		if addresses[i].IP == nil {
 			panic(addresses[i].IP)
@@ -28,33 +27,36 @@ func TestAllToAll(t *testing.T) {
 	n := 10
 	addresses := createAddresses(n)
 	fatal := make(chan error, 3*n)
-	var wg sync.WaitGroup
-	wg.Add(n)
 	for i := 0; i < n; i++ {
 		go func() {
-			defer wg.Done()
-			p := Player{Rank: i, Addresses: addresses}
-			actual, err := p.AllToAll(strconv.Itoa(i))
+			p, err := NewPlayer(i, addresses)
+			if err != nil {
+				fatal <- err
+				return
+			}
+			actual, err := p.AllToAll([]byte(strconv.Itoa(i)))
 			if err != nil {
 				fatal <- err
 				return
 			}
 			if len(actual) != n {
-				fatal <- fmt.Errorf("expected list of length %d, %v given", n, actual)
+				fatal <- fmt.Errorf("from player %d: expected list of length %d, %v given", i, n, actual)
 				return
 			}
 			for j := 0; j < n; j++ {
-				if strings.Compare(strconv.Itoa(j), actual[j]) != 0 {
-					fatal <- fmt.Errorf("expected %d, actual %v", j, actual[j])
+				if strconv.Itoa(j) != string(actual[j]) {
+					fatal <- fmt.Errorf("from player %d: expected %d, actual %v", i, j, actual[j])
 					return
 				}
 			}
+			fatal <- nil
 		}()
 	}
-	wg.Wait()
-	close(fatal)
-	for err := range fatal {
-		t.Error(err)
+	for i := 0; i < n; i++ {
+		err := <-fatal
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 }
 
@@ -62,12 +64,13 @@ func TestBroadcast(t *testing.T) {
 	addresses := createAddresses(10)
 	root := 3
 	fatal := make(chan error, 10)
-	var wg sync.WaitGroup
-	wg.Add(10)
 	for i := 0; i < 10; i++ {
 		go func(i int) {
-			defer wg.Done()
-			p := Player{Rank: i, Addresses: addresses}
+			p, err := NewPlayer(i, addresses)
+			if err != nil {
+				fatal <- err
+				return
+			}
 			a := strconv.Itoa(10 * i)
 			time.Sleep(time.Millisecond * 100 * time.Duration(p.Rank))
 			recv, err := p.Broadcast(a, root)
@@ -85,12 +88,14 @@ func TestBroadcast(t *testing.T) {
 				fatal <- fmt.Errorf("expected %d, actual %d", actual, root*10)
 				return
 			}
+			fatal <- nil
 		}(i)
 	}
-	wg.Wait()
-	close(fatal)
-	for err := range fatal {
-		t.Error(err)
+	for i := 0; i < 10; i++ {
+		err := <-fatal
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 }
 
@@ -98,27 +103,30 @@ func TestBroadcastBarrier(t *testing.T) {
 	addresses := createAddresses(10)
 	fatal := make(chan error, 10)
 	clocks := make(chan int, 20)
-	var wg sync.WaitGroup
-	wg.Add(10)
 	for i := 0; i < 10; i++ {
 		go func(i int) {
-			defer wg.Done()
-			p := Player{Rank: i, Addresses: addresses}
+			p, err := NewPlayer(i, addresses)
+			if err != nil {
+				fatal <- err
+				return
+			}
 			time.Sleep(time.Millisecond * 100 * time.Duration(p.Rank))
 			clocks <- 0
-			_, err := p.Broadcast("", 0)
+			_, err = p.Broadcast("", 0)
 			time.Sleep(time.Millisecond * 100 * time.Duration(p.Rank))
 			clocks <- 1
 			if err != nil {
 				fatal <- err
 				return
 			}
+			fatal <- nil
 		}(i)
 	}
-	wg.Wait()
-	close(fatal)
-	for err := range fatal {
-		t.Error(err)
+	for i := 0; i < 10; i++ {
+		err := <-fatal
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 	close(clocks)
 	prev := 0
@@ -140,21 +148,20 @@ func TestAllToAllBarrier(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		go func(i int) {
 			defer wg.Done()
-			p := Player{Rank: i, Addresses: addresses}
+			p, err := NewPlayer(i, addresses)
+			if err != nil {
+				fatal <- err
+				return
+			}
 			time.Sleep(time.Millisecond * 100 * time.Duration(p.Rank))
 			clocks <- 0
-			_, err := p.AllToAll("")
+			_, err = p.AllToAll([]byte{})
 			time.Sleep(time.Millisecond * 100 * time.Duration(p.Rank))
 			if err != nil {
 				fatal <- err
 				return
 			}
-			_, err = p.AllToAll("")
 			clocks <- 1
-			if err != nil {
-				fatal <- err
-				return
-			}
 		}(i)
 	}
 	wg.Wait()
