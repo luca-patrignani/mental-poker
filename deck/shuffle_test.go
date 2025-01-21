54109eda569e1a52c3ebf089f099ecb59ab400b7
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2025-01-21 22:09:23 +0100

    test: Add test to Shuffle

---
+++ deck/shuffle_test.go
@@ -0,0 +1,55 @@
+package deck
+
+import (
+	"testing"
+
+	"github.com/luca-patrignani/mental-poker/common"
+	"go.dedis.ch/kyber/v3"
+)
+
+
+func TestShuffle(t *testing.T) {
+	n := 10
+	addresses := common.CreateAddresses(n)
+	errChan := make(chan error)
+	decks := make(chan []kyber.Point, n)
+	for i := 0; i < n; i++ {
+		go func() {
+			deck := Deck{
+				DeckSize: 52,
+				Peer: common.NewPeer(i, addresses),
+			}
+			defer deck.Peer.Close()
+			_, err := deck.PrepareDeck()
+			if err != nil {
+				errChan <- err
+				return
+			}
+			err = deck.Shuffle()
+			if err != nil {
+				errChan <- err
+				return
+			}
+			decks <- deck.EncryptedDeck
+			errChan <- nil
+		}()
+	}
+	for i := 0; i < n; i++ {
+		err := <-errChan
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	close(decks)
+	deck := <- decks
+	for d := range decks {
+		if len(d) != 53 {
+			t.Fatal(len(d))
+		}
+		for i := range d {
+			if !deck[i].Equal(d[i]) {
+				t.Fatal(deck, d)
+			}
+		}
+	}
+}
