915d28072e4569253d54eee1840d88c6c648be93
Marco Galeri <marco.galeri@studio.unibo.it>
2025-02-06 18:38:16 +0100

    test: :sparkles: Implement winnerEvaluation func and poker_test

--- poker/poker.go
+++ poker/poker.go
@@ -1,23 +1,27 @@
 package poker
 
 import (
-	//"github.com/luca-patrignani/mental-poker/deck"
+	"bytes"
+	"encoding/binary"
+	"sort"
+
+	"github.com/luca-patrignani/mental-poker/deck"
 	"github.com/paulhankin/poker"
 )
 
-// TODO: Add a struct for holding hand and river card
 // Deck is the rappresentation of a game session.
-type PokerHand struct {
+type Session struct {
 	Board [5]poker.Card
 	Hand  [2]poker.Card
+	Deck deck.Deck
 }
+//TODO: Add interface for card, matching card struct of the package (non so come fare lucone :c)
 
-//TODO: Add interface for card matching card struct of the package
 
-// Convert the raw input card with the following suit order: ♣ -> ♦ -> ♥ -> ♠
+// Convert the raw input card with the following suit order: ♣clubs -> ♦diamonds -> ♥hearts -> ♠spades
 func convertCard(rawCard int) (poker.Card, error) {
-	suit := poker.Suit(uint8((rawCard / 13) + 1))
-	rank := poker.Rank((rawCard - 1) % 13)
+	suit := poker.Suit(uint8((rawCard / 13)))
+	rank := poker.Rank(((rawCard - 1) % 13)+1)
 	card, err := poker.MakeCard(suit, rank)
 	if err != nil {
 		return 0, err
@@ -26,8 +30,53 @@ func convertCard(rawCard int) (poker.Card, error) {
 }
 
 //Evaluate the final hand and return the peer rank of the winner
-func (hb *PokerHand) FinalHandEvaluation()([]int,error) {
-	finalHand := append(hb.Hand,hb.Board...)
-	score := poker.Eval7(finalHand)
-	//TODO: broadcast all to all to determine best
+func (hb *Session) WinnerEval()([]int,error) {
+	
+	playerNum := len(hb.Deck.Peer.Addresses)
+
+	var finalHand [7]poker.Card
+	copy(finalHand[:5],hb.Board[:])
+	copy(finalHand[5:],hb.Hand[:])
+	score := poker.Eval7(&finalHand)
+
+	//Marshall the data
+	bufferSend := new(bytes.Buffer)
+	binary.Write(bufferSend,binary.BigEndian, score)
+
+	byteScores, err := hb.Deck.Peer.AllToAll(bufferSend.Bytes())
+	if err != nil {
+		return []int{-1},err
+	}
+	//Unmarshal the data
+	var scores []int16
+	for i := 0; i < len(byteScores); i++ {
+		scores[i] = int16(binary.BigEndian.Uint16(byteScores[i]))
+	}
+
+	// Create a slice of player indexes
+	players := make([]int, playerNum)
+	for i := range players {
+		players[i] = i
+	}
+
+	// Sort indexes based on scores
+	sort.Slice(players, func(i, j int) bool {
+		return scores[players[i]] > scores[players[j]]
+	})
+
+	sort.Slice(scores, func(i, j int) bool {
+		return scores[i] < scores[j]
+	})
+
+	// Check for ties 
+	winner := []int{players[0]}
+	for i:=0; i<len(scores); i++ {
+		if scores[i] == scores[i+1] {
+			winner = append(winner,players[i])
+		} else {
+			i = len(scores)
+		}
+	}
+
+	return winner,nil
 }
--- poker/poker_test.go
+++ poker/poker_test.go
@@ -4,8 +4,128 @@ import (
 	//"errors"
 	//"fmt"
 	"testing"
+
+	"github.com/luca-patrignani/mental-poker/common"
+	"github.com/luca-patrignani/mental-poker/deck"
+	"github.com/paulhankin/poker"
 )
 
+//TODO: make more elaborate test
 func TestConvertCard(t *testing.T) {
+	jackClub, _ := poker.MakeCard(poker.Club,11)
+	testCard,err := convertCard(11)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !testCard.Valid() {
+		t.Fatal("card not valid")
+	}
+	if testCard != jackClub {
+		errString := "expected "+jackClub.String()+", get "+testCard.String()
+		t.Fatal(errString)
+	}
+
+}
+func TestWinnerEval(t *testing.T) {
+	n := 10
+	addresses := common.CreateAddresses(n)
+	errChan := make(chan error)
+	winChan := make(chan []int, 10)
+	for i := 0; i < n; i++ {
+		deck := deck.Deck{
+			DeckSize: 52,
+			Peer:     common.NewPeer(i, addresses),
+		}
+		session := Session{
+			Board: [5]poker.Card{},
+			Hand: [2]poker.Card{},
+			Deck: deck,
+		}
+		defer deck.Peer.Close()
+		// TODO: Si blocca qua non so il perchè T-T 
+		err := deck.PrepareDeck()
+		if err != nil {
+			errChan <- err
+			return
+		}
+		err = deck.Shuffle()
+		if err != nil {
+			errChan <- err
+			return
+		}
+
+		for drawer:=0; drawer < n; drawer++ {
+			cardA, err := deck.DrawCard(drawer)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			cardB, err := deck.DrawCard(drawer)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			cardConvA,err := convertCard(cardA)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			cardConvB,err := convertCard(cardB)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			if i == drawer {
+				session.Hand[0] = cardConvA
+				session.Hand[1] = cardConvB
+				t.Logf("Player %d got %s and %s",drawer,cardConvA.String(),cardConvB.String())
+			}
+		}
+		for board:=0; board<5; board++ {
+			card, err := deck.DrawCard(0)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			card, err = deck.OpenCard(0,card)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			cardRev, err := convertCard(card)
+
+			if err != nil {
+				errChan <- err
+				return
+			}
+			session.Board[board] = cardRev
+			t.Logf("board for player %d\n%s",i, append(session.Board[:],session.Hand[:]...))
+		}
+		winner, err := session.WinnerEval()
+		if err != nil {
+			errChan <- err
+			return
+		}
+		winChan <- winner[:]
+		//var finalHand [7]poker.Card
+		//copy(finalHand[:5],session.Board[:])
+		//copy(finalHand[5:],session.Hand[:])
+		//score := poker.Eval7(&finalHand)
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-errChan
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	//winner := <-winChan
+	//for i := 1; i < n; i++ {
+	//	win := <-winChan
+	//	for j := 
+	//	if winner != win {
+	//		t.Fatal(, c)
+	//	}
+	//}
 
 }
